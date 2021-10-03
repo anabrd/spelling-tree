e1e43b90f56f8ee61dd565971dc274229326b0eb
Ana Brdar <ana.d.brdar@gmail.com>
2021-10-03 19:59:51 +0200

    add letters

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -11,7 +11,7 @@ let containerSize: number = 2*r+imgSize;
 interface LeafObj {
     isCenter?: boolean;
     letter?: string;
-    position?: number;
+    position?: number | null;
     src?: string;
 }
 
@@ -33,20 +33,74 @@ const Leaf = styled.img<LeafObj>`
     translate(${r}px)
 `
 
+const LeafCenter = styled.div<LeafObj>`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-color: white;
+    border: 4px solid black;
+    border-radius: 50%;
+    margin: ${imgSize * -0.5}px;
+    height: ${imgSize}px;
+    width: ${imgSize}px;
+    position: absolute;
+`
+
+const LetterWrapper = styled.div<LeafObj>`
+    top: 50%;
+    left: 50%;
+    margin: ${imgSize * -0.5}px;
+    height: ${imgSize}px;
+    width: ${imgSize}px;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    z-index: 10;
+    transform: 
+    rotate(calc(${props => props.position}*1turn/6/-1.67))
+    translate(${r}px)
+`
+
+const Letter = styled.p<LeafObj>`
+    font-size: 32px;
+    font-weight: 700;
+    transform: 
+    ${props => props.isCenter ? null : "rotate(90deg)"};
+    display: inline-block;
+`
+
 function Tree() {
     const leafArrDupe: LeafObj[] = [];
     let leafRenderArr: JSX.Element[] = [];
     const [leafArr, setLeafArr] = useState<LeafObj[]>([]);
 
     for (let i = 0; i < 6; i++) {
-        leafArrDupe.push({isCenter: false, position: i})
+        if (i == 3) {
+            leafArrDupe.push({isCenter: true, letter: "b"})
+            leafArrDupe.push({isCenter: false, position: i, letter: "a"})
+        } else {
+            leafArrDupe.push({isCenter: false, position: i, letter: "a"})
+        }
     }
 
     useEffect(() => {
         setLeafArr([...leafArrDupe])
     }, [])
 
-    leafRenderArr = leafArr.map((leafRend, index) => <Leaf key={index} src={leafImg} position={leafRend.position} isCenter={leafRend.isCenter}/>)
+    leafRenderArr = leafArr.map((leafRend, index) => 
+    leafRend.isCenter ?
+    <LeafCenter>
+        <LetterWrapper isCenter={leafRend.isCenter} position={leafRend.position}>
+            <Letter isCenter={leafRend.isCenter}>{leafRend.letter}</Letter>
+        </LetterWrapper>
+    </LeafCenter>
+    :
+    <>
+        <LetterWrapper position={leafRend.position}><Letter>{leafRend.letter}</Letter></LetterWrapper>
+        <Leaf key={index} src={leafImg} position={leafRend.position} isCenter={leafRend.isCenter}/>
+    </>)
 
     return (
         <TreeWrapper>
